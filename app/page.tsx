19efd9385228f1136d359d481733ca3d3a3a019e
Adriel Lucas <adriel.agco@gmail.com>
2025-08-09 23:12:38 -0400

    init spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Search } from "lucide-react";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="flex items-center justify-center h-screen p-2">
+      <div className="relative w-full flex items-center gap-4 px-4 h-full border-1 border-zinc-200/10 bg-zinc-800/95 shadow-md rounded-2xl">
+        <Search size={30} className="text-zinc-400/40"/>
+        <input type="text" placeholder="Spotlight Search" className="text-zinc-200 placeholder:text-zinc-200/30 outline-none text-xl text-left w-full" />
+      </div>
     </main>
   );
 }
